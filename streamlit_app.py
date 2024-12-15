bf2c0245db604844fd306d320c71e52041282f6f
adomfosugit <115159872+adomfosugit@users.noreply.github.com>
2024-12-15 19:14:41 +0000

    heap them

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,20 +3,13 @@ import numpy as np
 import pandas as pd
 import pickle
 from copy import deepcopy as dc
-
-
-
 st.title('Estimate BHP')
 st.subheader("Upload your CSV file here")
-
-# Define the required columns
 required_columns = ['PRODUCTION DATE', 'Qliquid', 'GOR', 'Pwh', 'THT', 'WCT']
-
-# File upload
 uploaded_file = st.file_uploader("Choose a file")
-
-# Function to prepare data for LSTM
 def prepare_dataframe_for_lstm(df, n_steps):
+    date = df['PRODUCTION DATE']
+    date = date[2]
     df = dc(df)
     df.set_index('PRODUCTION DATE', inplace=True)
 
@@ -49,19 +42,16 @@ if uploaded_file is not None:
 
 # Load the saved model and scaler
 def load_model():
-    with open('saved_steps98.pkl', 'rb') as file:
+    with open('modelXAMATR_MODEL.pkl', 'rb') as file:
         data = pickle.load(file)
     return data
 
 data = load_model()
 model = data['model']
-def load_scaler():
-    with open('scaler.pkl', 'rb') as file:
-        scale = pickle.load(file)
-    return  scale
+scaler = data['scaler']
 
-scale1 = load_scaler()
-scaler = scale1['scaler']
+
+
 def manual_unscale(scaled_data):
     # Extract the mean and standard deviation from the scaler
     means = 3255
@@ -79,7 +69,7 @@ def start_prediction():
         df = st.session_state['dataframe']
         
         # Apply scaling to the necessary columns
-        columns_to_scale = ['Qliquid', 'GOR', 'Pwh', 'THT','WCT','Qliquid(t-1)','GOR(t-1)','Pwh(t-1)','THT(t-1)',	'WCT(t-1)','Qliquid(t-2)','GOR(t-2)','Pwh(t-2)','THT(t-2)',	'WCT(t-2)']
+        columns_to_scale = ['MBHFP','Qliquid', 'GOR', 'Pwh', 'THT','WCT','Qliquid(t-1)','GOR(t-1)','Pwh(t-1)','THT(t-1)','WCT(t-1)','Qliquid(t-2)','GOR(t-2)','Pwh(t-2)','THT(t-2)',	'WCT(t-2)']
 
         # Ensure these columns exist in the DataFrame
         scaled_columns = [col for col in columns_to_scale if col in df.columns]
@@ -89,7 +79,8 @@ def start_prediction():
         scaled_df = pd.DataFrame(data_predicted, columns=scaled_columns)
 
         # Prepare the input for the LSTM
-        X = scaled_df.values.reshape((scaled_df.shape[0], 5, 3))  # Adjust dimensions as necessary
+        X= scaled_df[['Qliquid', 'GOR', 'Pwh', 'THT','WCT','Qliquid(t-1)','GOR(t-1)','Pwh(t-1)','THT(t-1)','WCT(t-1)','Qliquid(t-2)','GOR(t-2)','Pwh(t-2)','THT(t-2)',	'WCT(t-2)']]
+        X = X.values.reshape((scaled_df.shape[0], 5, 3))  # Adjust dimensions as necessary
         y_pred = model.predict(X)
         y_pred = y_pred.reshape(1,-1)
         y_pred_1 = y_pred.reshape(-1)
@@ -111,7 +102,5 @@ if 'prediction_result' in st.session_state:
 
     # Create a pandas DataFrame for the prediction results to use with st.linechart
     prediction_df = pd.DataFrame(st.session_state['prediction_result'], columns=['Predicted BHP'])
-    
-    # Plotting the prediction result using Streamlit's linechart
-    st.line_chart(prediction_df)
+
 
